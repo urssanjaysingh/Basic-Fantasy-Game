7dd4bf7cde2787997cc0b3cee294c7145be6b3e2
urssanjaysingh <sanjaysingh26112000@gmail.com>
2024-10-21 14:55:47 +0530

    build

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://basic-fantasy-game.onrender.com",
+	// frontend base url
+    baseURL: "https://basic-fantasy-game.onrender.com", 
 });
 
 export default instance;
